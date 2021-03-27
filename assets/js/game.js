eb49d307333b58489ecda8a02da68d2ca3ca5f9d
natasharrison <natasharrison@gmail.com>
2021-03-26 20:48:57 -0700

    codesnap shot errors

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,6 +5,7 @@ var randomNumber = function(min, max) {
     return value;
 };
 
+// FUNCTION to check if player wants to fight or skip
 var fightOrSkip = function() {
     // ask player if they'd like to fight or skip using fightOrSkip function 
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
@@ -14,6 +15,7 @@ var fightOrSkip = function() {
         window.alert("You need to provide a valid answer! Please try again.");
         return fightOrSkip();
     }
+
     promptFight = promptFight.toLowerCase();
     // if player picks "skip" confirm and then stop the loop
     if (promptFight === "skip") {
@@ -24,14 +26,15 @@ var fightOrSkip = function() {
         if (confirmSkip) {
             window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
             // subtract money from playerMoney for skipping 
-            playerInfo.playerMoney = Math.max(0, playerInfo.money - 10);
-            
+            playerInfo.money = Math.max(0, playerInfo.money - 10);
+            // stop while() loop using break; and enter next fight
+
             // return true if player wants to leave
             return true; 
         }
     }
     return false; 
-}
+};
 
 // FIGHT FUNCTION 
 var fight = function(enemy) {
@@ -145,12 +148,20 @@ for(var i=0; i < enemyInfo.length; i++) {
 var endGame = function() {
     window.alert("The game has now ended. Let's see how you did!");
 
-    // if player is still alive, player wins! 
-    if (playerInfo.health > 0) {
-        window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + ".");
+    // check local storage for high score, if it's not there, use 0
+    var highScore = localStorage.getItem("highscore");
+    if (highScore === null) {
+        highScore = 0;
+    }
+
+    if (playerInfo.money > highScore) {
+        localStorage.setItem("highscore", playerInfo.money);
+        localStorage.setItem("name", playerInfo.name);
+
+        alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
     } else {
-        window.alert("You have lost your robot in battle!");
-     }
+        alert(playerInfo.name + "did not beat the high score of " + highScore + ". Maybe next time!");
+    }
 
     // ask player if they'd like to play again 
     var playAgainConfirm = window.confirm("Would you like to play again?");
